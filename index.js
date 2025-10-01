64ea6a76be0366d0b2590069b73a96b1175f4d57
Cole Peterson <colepeterson05@icloud.com>
2025-10-01 14:02:58 -0400

    Started JS File
    While in class I started studying JS. During this class I added a JS file to the project. Its just simple variables and this class will change into something implementable to the project.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Happy developing ✨')
+console.log("Hello World, from external JS!"); //First line in JS
+
+let age = 20;
+let name = 'Cole';
+console.log("name is: " + name + ", age is: " + age);
+
+let a = 5;
+console.log(a);
+let b = 20;
+console.log(b);
+
+let msg = "hello world";
+                //012345678910
+console.log(msg);
+console.log(msg.charAt(4)); //returns charcter at indext 4
+console.log(msg.replace('o','a')); //replacing o with a
+
+console.log(msg.split(" "));
+
+console.log(' ' == '0'); //comparison function
