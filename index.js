e25b911047e7fbbd2a8fdb905e8336d620b9d5ae
Cole Peterson <colepeterson05@icloud.com>
2025-11-09 20:02:11 -0500

    Added Carousel
    +added JS Carousel (temp images) for hosting weekly pizzas and such.
    
    +Next task is fixing the mouse image
    
    +Added index.js to main file

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const follower = document.getElementById('follower');
 document.addEventListener('mousemove', (e) => {
     follower.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
+
+console.log("JAVASCRIPT IS WORKING HORRAY 🥲");
+'use strict';
+
+const carouselItems = document.querySelectorAll('.carousel-item');
+const navItems = document.querySelectorAll('.nav-item');
+const nav = document.querySelector('.carousel-nav');
+
+nav.addEventListener('click', function (e) {
+    if (!e.target.classList.contains('nav-item')) return;
+
+    const clickedDot = e.target;
+    const activeDot = document.querySelector('.nav-item.active');
+
+    if (clickedDot === activeDot) return;
+
+    const newIndex = Array.from(navItems).indexOf(clickedDot);
+    const activeIndex = Array.from(navItems).indexOf(activeDot);
+
+    carouselItems[activeIndex].classList.remove('active');
+    activeDot.classList.remove('active');
+
+    carouselItems[newIndex].classList.add('active');
+    clickedDot.classList.add('active');
+});
